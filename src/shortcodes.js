b30f3004e27fcdcc308f02df867ada6f2edf03a1
Aravind Reddy Voggu <zerodividedby0@gmail.com>
2023-06-20 01:00:50 +0530

    Add build time to about page

--- src/shortcodes.js
+++ src/shortcodes.js
@@ -3,18 +3,37 @@ const postcss = require('../postcss.config.js')
 
 
 const cssconcat = async function () {
-    const css = await postcss.process('assets/tailwind.css') + ''
+    const css = await postcss.process('assets/tailwind.css') + '';
 
-    return css
-}
+    return css;
+};
 
-const scriptconcat = async function() {
-    const script = await fs.readFile('assets/index.js')
 
-    return script
-}
+const istDateString = function () {
+    // Get current date and time
+    const now = new Date();
+
+
+    // Define options for formatting
+    const options = {
+        timeZone: 'Asia/Kolkata', // IST timezone
+        hour12: false, // 24-hour format
+        weekday: 'long', // full weekday name
+        year: 'numeric', // 4-digit year
+        month: 'long', // full month name
+        day: 'numeric', // day of the month
+        hour: 'numeric', // hour (0-23)
+        minute: 'numeric', // minute
+        second: 'numeric' // second
+    };
+
+    // Convert to IST string representation
+    const istDateString = now.toLocaleString('en-IN', options);
+
+    return istDateString;
+};
 
 module.exports = config => {
-    config.addNunjucksAsyncShortcode('cssconcat', cssconcat)
-    config.addNunjucksAsyncShortcode('scriptconcat', scriptconcat)
+    config.addNunjucksAsyncShortcode('cssconcat', cssconcat);
+    config.addShortcode('istDateString', istDateString);
 }
